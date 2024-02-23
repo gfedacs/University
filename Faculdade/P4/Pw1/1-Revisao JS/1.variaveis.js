8272ba4e2eb41f2c4242cb98562c7a9cd0159d42
Gabriel Félix <gfedacs@hotmail.com>
2024-02-23 11:14:15 -0300

    aula01

---
+++ Faculdade/P4/Pw1/1-Revisao JS/1.variaveis.js
@@ -0,0 +1,25 @@
+let numero = 1;
+
+
+
+while (numero <=10){
+    numero = numero + numero;
+    console.log(numero)
+}
+
+console.log(numero);
+console.log("----------------");
+console.log(typeof "javascript")
+console.log(typeof 5.13)
+console.log(typeof true)
+console.log("----------------");
+
+
+//let nome = prompt("Digite seu nome:");
+
+//alert("dale")
+
+
+
+
+
